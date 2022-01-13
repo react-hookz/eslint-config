d0e5ad18eb5d98fc49beff8ddc59f16f6dc89730
xobotyi <xog3@yandex.ru>
2022-01-13 14:01:13 +0300

    fix: improve mdx config

--- mdx.js
+++ mdx.js
@@ -1,32 +1,55 @@
+const prettierCfg = {
+  printWidth: 100,
+  tabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  endOfLine: 'lf',
+  tabWidth: 2,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'always',
+  proseWrap: 'always',
+};
+
 module.exports = {
-  extends: ['./base'],
+  extends: ['plugin:mdx/recommended', 'plugin:prettier/recommended'],
+
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+      jsx: true,
+    },
+  },
+
+  settings: {
+    'mdx/code-blocks': false,
+  },
+
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        ...prettierCfg,
+        parser: 'mdx',
+      },
+    ],
+  },
 
   overrides: [
     {
-      files: ['*.md', '*.mdx'],
-      extends: ['plugin:mdx/recommended', 'plugin:mdx/overrides', 'plugin:prettier/recommended'],
+      files: ['*.md'],
       rules: {
         'prettier/prettier': [
-          2,
+          'error',
           {
-            printWidth: 100,
-            tabs: false,
-            semi: true,
-            singleQuote: true,
-            trailingComma: 'es5',
-            endOfLine: 'lf',
-            tabWidth: 2,
-            bracketSpacing: true,
-            bracketSameLine: true,
-            arrowParens: 'always',
+            ...prettierCfg,
             parser: 'markdown',
           },
         ],
       },
-    },
-    {
-      files: '**/*.{md,mdx}/**',
-      extends: 'plugin:mdx/code-blocks',
     },
   ],
 };
