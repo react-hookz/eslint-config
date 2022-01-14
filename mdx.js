286e560592d437343c223f7d0c9937136acf2021
Kyle Holmberg <inbox@kylemh.com>
2022-01-14 19:04:08 +0700

    Remove prettier config from md/mdx ESLint config

--- mdx.js
+++ mdx.js
@@ -1,17 +1,3 @@
-const prettierCfg = {
-  printWidth: 100,
-  tabs: false,
-  semi: true,
-  singleQuote: true,
-  trailingComma: 'es5',
-  endOfLine: 'lf',
-  tabWidth: 2,
-  bracketSpacing: true,
-  bracketSameLine: true,
-  arrowParens: 'always',
-  proseWrap: 'always',
-};
-
 module.exports = {
   extends: ['plugin:mdx/recommended', 'plugin:prettier/recommended'],
 
@@ -32,7 +18,6 @@ module.exports = {
     'prettier/prettier': [
       'error',
       {
-        ...prettierCfg,
         parser: 'mdx',
       },
     ],
@@ -45,7 +30,6 @@ module.exports = {
         'prettier/prettier': [
           'error',
           {
-            ...prettierCfg,
             parser: 'markdown',
           },
         ],
