b6b442977c8b1f5003d3f0dab8614040ab1e37bc
xobotyi <xog3@yandex.ru>
2023-06-30 10:49:55 +0200

    fix: add rules to base and react configs

--- react.cjs
+++ react.cjs
@@ -1,3 +1,8 @@
 module.exports = {
 	extends: ['xo-react', 'plugin:prettier/recommended'],
+	rules: {
+		// While using ts with `react-jsx` preset - there
+		// is no need in importing react in each file
+		'react/react-in-jsx-scope': 'off',
+	},
 };
