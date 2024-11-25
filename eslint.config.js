b991fb30059956b64d07a1b44e2309092407d514
xobotyi <xog3@yandex.ru>
2024-11-25 17:42:38 +0100

    chore: reconfigure existing linting to esl9

---
+++ eslint.config.js
@@ -0,0 +1,4 @@
+import baseConfig from './configs/base.js';
+import mdConfig from './configs/md.js';
+
+export default [baseConfig, mdConfig];
--- jest.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	extends: ['plugin:jest/recommended', 'plugin:prettier/recommended'],
-	rules: {
-		'max-nested-callbacks': ['warn', 10],
-	},
-};
