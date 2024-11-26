01cd9a8640f8c5797b1845da9bef0453c2964417
xobotyi <xog3@yandex.ru>
2024-11-26 10:07:13 +0100

    fix: remove nonexistent rules from typescript config

--- configs/typescript.js
+++ configs/typescript.js
@@ -50,55 +50,6 @@ const typescriptConfig = [
 					prefer: 'type-imports',
 				},
 			],
-			'@typescript-eslint/ban-types': [
-				'error',
-				{
-					extendDefaults: false,
-					types: {
-						String: {
-							message: 'Use `string` instead.',
-							fixWith: 'string',
-						},
-						Number: {
-							message: 'Use `number` instead.',
-							fixWith: 'number',
-						},
-						Boolean: {
-							message: 'Use `boolean` instead.',
-							fixWith: 'boolean',
-						},
-						Symbol: {
-							message: 'Use `symbol` instead.',
-							fixWith: 'symbol',
-						},
-						BigInt: {
-							message: 'Use `bigint` instead.',
-							fixWith: 'bigint',
-						},
-						Object: {
-							message:
-								'The `Object` type is mostly the same as `unknown`. You probably want `Record<string, unknown>` instead. See https://github.com/typescript-eslint/typescript-eslint/pull/848',
-							fixWith: 'Record<string, unknown>',
-						},
-						'{}': {
-							message:
-								'The `{}` type is mostly the same as `unknown`. You probably want `Record<string, unknown>` instead.',
-							fixWith: 'Record<string, unknown>',
-						},
-						object: {
-							message:
-								'The `object` type is hard to use. Use `Record<string, unknown>` instead. See: https://github.com/typescript-eslint/typescript-eslint/pull/848',
-							fixWith: 'Record<string, unknown>',
-						},
-						Function: 'Use a specific function type instead, like `() => void`.',
-						'[[]]':
-							"Don't use `[[]]`. It only allows an array with a single element which is an empty array. Use `SomeType[][]` instead.",
-						'[[[]]]': "Don't use `[[[]]]`. Use `SomeType[][][]` instead.",
-						'[[[[]]]]': 'ur drunk 🤡',
-						'[[[[[]]]]]': '🦄💥',
-					},
-				},
-			],
 		},
 	},
 
