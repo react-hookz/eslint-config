a99183c8aa8dd9a890f2f0d5b3c9ec2bbdb9e4d5
xobotyi <xog3@yandex.ru>
2024-12-08 15:28:40 +0100

    fix: disable codeblocks checks for md and mdx

--- configs/md.js
+++ configs/md.js
@@ -2,27 +2,7 @@ import * as mdx from 'eslint-plugin-mdx';
 
 /** @type {Linter.Config[]} */
 const mdConfig = [
-	{
-		...mdx.flat,
-		// Optional, if you want to lint code blocks at the same
-		processor: mdx.createRemarkProcessor({
-			lintCodeBlocks: true,
-			// Optional, if you want to disable language mapper, set it to `false`
-			// if you want to override the default language mapper inside, you can provide your own
-			languageMapper: {},
-		}),
-	},
-	{
-		...mdx.flatCodeBlocks,
-		rules: {
-			...mdx.flatCodeBlocks.rules,
-			// If you want to override some rules for code blocks
-			'no-var': 'error',
-			'prefer-const': 'error',
-			'import/no-unresolved': 'off',
-			'import/no-anonymous-default-export': 'off',
-		},
-	},
+	mdx.flat,
 ];
 
 export default mdConfig;
